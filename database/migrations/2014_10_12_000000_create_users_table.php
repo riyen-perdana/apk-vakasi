d1c3b9c54c473fae6015ca884c723a61fc2f14a2
riyenperdana <riyenperdana@uin-suska.ac.id>
2023-10-30 11:45:31 +0700

    commit

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('nip',16)->comment('Nomor Induk Pegawai')->unique();
+            $table->string('glr_dpn')->comment('Gelar Depan')->nullable();
+            $table->string('glr_blk')->comment('Gelar Belakang')->nullable();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->enum('is_aktif',['Y','N'])->comment('Apakah Aktif ?')->default('Y');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
